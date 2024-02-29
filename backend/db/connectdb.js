c69cbc50ac5c4eed94163c63dee441fb4e46b87e
unknown <shahnawaz>
2024-02-29 16:11:20 +0530

    indentation

--- backend/db/connectdb.js
+++ backend/db/connectdb.js
@@ -1,14 +1,3 @@
-// import mongoose from "mongoose";
-
-// export const connectdb=async()=>{
-//     try {
-//         await mongoose.connect(process.env.MONGO_DB_URI,)
-//         console.log("connected successfully>>>");
-//     } catch (error) {
-//         console.log("connection failed<<<");
-//     }
-// }
-
 import mongoose from "mongoose";
 
 const connectdb = async () => {
